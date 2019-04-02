f5990dc21ab168080290500c7dc74a6637f7c03d
michael-ptitsyn <ptitsyn@post.bgu.ac.il>
2019-04-02 23:11:13 +0300

    new const timeout

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -1,4 +1,5 @@
 public class Constants {
     public static String JAVA8IMG = "ami-0b8962a37670b6d05";
     public static final String DEFAULT_REGION="us-east-1";
+    public static final int TIMEOUT=10;
 }
